6d23a80fef711611e8cd8d3d27c7f3c75df59986
Marek Reid <marekreid@Mareks-MacBook-Pro.local>
2018-11-15 07:11:19 +0100

    Updates

---
+++ precache-manifest.ab6db720ae2f78be5d8b7bd129b3c08e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71d08d3a363092d4fee0",
+    "url": "/REACT-TodoList/static/js/runtime~main.71d08d3a.js"
+  },
+  {
+    "revision": "fbdb64859ef4db8c94ec",
+    "url": "/REACT-TodoList/static/js/main.fbdb6485.chunk.js"
+  },
+  {
+    "revision": "eaa4f97808dbb25af475",
+    "url": "/REACT-TodoList/static/js/1.eaa4f978.chunk.js"
+  },
+  {
+    "revision": "fbdb64859ef4db8c94ec",
+    "url": "/REACT-TodoList/static/css/main.56f3fa6f.chunk.css"
+  },
+  {
+    "revision": "87e6fbfdd889b1d8671adcaec408b293",
+    "url": "/REACT-TodoList/index.html"
+  }
+];
